578f760d8f6c04e7f50a11dc4d554593035bdce4
iChou <me@ichou.cn>
2014-12-06 04:11:53 +0800

    fix a bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 # Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/,/.*\.gif/]
-
 Rails.application.config.assets.precompile += %w( jquery-fileupload-4-yii.js )
