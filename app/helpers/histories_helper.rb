3db0f23c5e9d78afed4683cc722130b17ac8b6ca
iChou <me@ichou.cn>
2014-12-06 22:27:34 +0800

    diff title

--- app/helpers/histories_helper.rb
+++ app/helpers/histories_helper.rb
@@ -5,7 +5,7 @@ module HistoriesHelper
     o = h.original['content']
     m = h.modified['content']
     html = Diffy::Diff.new(o, m || " ", diff: ['-u', '-d', "-L#{l1}", "-L#{l2}"], include_diff_info: true, include_plus_and_minus_in_html: false, allow_empty_diff: true).to_s(:html)
-    # html = html.scan(/(.*<\/ins><\/li>)\s*<li/m)[0][0] + "</ul></div>" unless m
+    html = html.sub(/<li class="ins"><ins> <\/ins><\/li>/, '') unless m
     html.html_safe
   end
 end
