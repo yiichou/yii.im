6ad3d30bdfca29b83c6bae5d53109288cb7a07ab
iChou <me@ichou.cn>
2014-12-06 18:38:20 +0800

    be thiner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,14 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+
+//= require jquery-ui/autocomplete
+//= require jquery-ui/effect-blind
 
 //= require jquery-plugins/jquery.cookie
 //= require jquery-plugins/jquery.color
 //= require jquery-plugins/jquery.hotkeys
 //= require rails-timeago
 //= require bootstrap
-//= require highlight.min
-
-//= require codemirror
-//= require codemirror/modes/ruby
-//= require codemirror/modes/markdown
-//= require codemirror/modes/gfm
-//= require codemirror/modes/javascript
-//= require codemirror/modes/css
-//= require codemirror/modes/htmlmixed
-//= require codemirror/modes/shell
-//= require codemirror/modes/yaml
-//= require codemirror/addons/mode/overlay
 
 //= require showdown
 //= require tag-it
@@ -27,76 +17,6 @@
 jQuery(function ($) { $(document).ready(function(){
 
   var showdown = new Showdown.converter()
-
-  $('textarea').each(function (index, area) {
-    var preview = $($(area).data('preview'));
-    var editor = CodeMirror.fromTextArea(area, {
-      mode: 'gfm',
-      theme: 'default',
-      tabSize: 2,
-      autoFocus: false,
-      lineNumbers: false,
-      lineWrapping: true,
-      matchBrackets: true,
-    });
-
-    editor.on('change', function(cm, args) {
-      if (preview.length !== 0) {
-        preview.html(showdown.makeHtml(cm.getValue()));
-        hljs.tabReplace = '<span class="indent">\t</span>';
-        $('pre code', preview).each(function(i, e) {hljs.highlightBlock(e, hljs.tabReplace, false)});
-      }
-    });
-
-    $(document).on('click', '.editor-attach', function(e) {
-      e.preventDefault();
-      var _n = $(this).data('name');
-      var _c = editor.getCursor();
-      var _s = "[" + _n + "](" + $(this).data('url') + ")";
-      var _ext = _n.substr(_n.lastIndexOf('.') +1).toLowerCase();
-      switch (_ext) {
-        case 'jpg':
-        case 'jpeg':
-        case 'png':
-        case 'gif':
-          editor.replaceRange("!" + _s, _c, _c);
-          break;
-        default:
-          editor.replaceRange(_s, _c, _c);
-      }
-    });
-
-    $(document).on('content:clear', area, function(e) {
-      editor.setValue('');
-      editor.getTextArea().value = '';
-    });
-
-    var form = $(this).parents('form');
-
-    var formLocal = JSON.stringify(form.serializeArray());
-    var saveLocal = function (store) {
-      editor.getTextArea().value = editor.getValue();
-      var formCurrent = JSON.stringify(form.serializeArray());
-      if (formCurrent != formLocal) {
-        localStorage.setItem(form.attr('id'), formCurrent);
-        formLocal = formCurrent;
-      }
-    };
-    setInterval(saveLocal, 8000);
-
-    var formRemote = JSON.stringify(form.serializeArray());
-    var saveRemote = function (store) {
-      editor.getTextArea().value = editor.getValue();
-      var formCurrent = JSON.stringify(form.serializeArray());
-      if (formCurrent != formRemote) {
-        $.rails.handleRemote(form);
-        formRemote = formCurrent;
-      }
-    };
-    if (form.data('persisted')) {
-      setInterval(saveRemote, 480000);
-    }
-  });
 
   $('.tagit').tagit({
     allowSpaces: false,
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-# Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
-Rails.application.config.assets.precompile += %w( jquery-fileupload-4-yii.js )
+Rails.application.config.assets.precompile += %w( jquery-fileupload-4-yii.js codemirror-4-yii.js )
