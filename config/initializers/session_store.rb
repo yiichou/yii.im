5a29f197533914834b03948c820bcf1a31f100ac
iChou <me@ichou.cn>
2015-01-30 02:16:22 +0800

    mac check

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store, key: '_yii_im_session'
+# check if working om my mac ( under development).
+Rails.application.config.session_store :redis_store, key: '_yii_im_session' unless FileTest::exist?("/Users/ichou")
