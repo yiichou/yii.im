ed7c954d69d7d75ee25d81bf8a13f1ba61247fd8
周艺 <me@ichou.cn>
2016-06-18 11:33:26 +0800

    a new post

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "git push origin hugo"
 git push origin hugo
 echo "========================"
 echo "Deploy to yii"
-shell='cd /var/www/yii.im.hugo && git pull origin hugo && hugo -t slim'
+shell='cd /var/www/yii.im.hugo && git pull origin hugo && echo "========================" && hugo -t slim'
 ssh yii ${shell}
 echo "Status: Finished"
 echo "========================"
